2e50f76d6a4220957381ef15251c89d86c470aa5
SantyVill <santyvillagra@gmail.com>
2020-05-28 23:08:45 -0300

    Agrego la resolucion del Tp07

---
+++ Tp07/punto3/Calculadora.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace punto3
+{
+    class Calculadora
+    {
+        private float numero1;
+        private float numero2;
+         public float Numero1
+        {
+            get
+            {
+                return numero1;
+            }
+            set
+            {
+                numero1 = value;
+            }
+        }
+        public float Numero2
+        {
+            get
+            {
+                return numero2;
+            }
+            set
+            {
+                numero2 = value;
+            }
+        }
+
+        public float Suma()
+        {
+            return (numero1+numero2);
+        }
+        public float Resta()
+        {
+            return (numero1-numero2);
+        }
+        public float Multiplicacion()
+        {
+            return numero1 * numero2;
+        }
+        public float Division()
+        {
+            return numero1 / numero2;
+        }
+    }
+}
